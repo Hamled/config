d10c91ba858deaadcb57ec090f530552833c410e
Charles Ellis <hamled@hamled.dev>
2022-10-12 16:52:19 -0700

    Home manager base suite has direnv

--- flake.nix
+++ flake.nix
@@ -89,7 +89,7 @@
         modules = [ ];
         importables = rec {
           profiles = digga.lib.rakeLeaves ./profiles/home;
-          suites = with profiles; rec { base = [ ]; };
+          suites = with profiles; rec { base = [ direnv ]; };
         };
       };
       homeConfigurations =
