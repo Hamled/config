6a519e352a5db76d8e425b960e9c47bf9eea256e
Charles Ellis <hamled@hamled.dev>
2022-10-12 21:53:55 -0700

    NixOS test user has vi shell option

--- users/home/nixos.nix
+++ users/home/nixos.nix
@@ -1,4 +1,11 @@
 { suites, pkgs, ... }: {
   imports = suites.base;
   home.packages = with pkgs; [ hello htop nix-index ];
+
+  programs.bash = {
+    enable = true;
+    initExtra = ''
+      set -o vi
+    '';
+  };
 }
