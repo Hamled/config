b3aee14ecc0501e1262c9f4158f2106fc77306a7
Charles Ellis <hamled@hamled.dev>
2022-10-20 09:13:18 -0700

    NixOS user charles uses HM user charles

--- users/nixos/charles.nix
+++ users/nixos/charles.nix
@@ -1,4 +1,6 @@
-{ ... }: {
+{ hmUsers, ... }: {
+  home-manager.users = { inherit (hmUsers) charles; };
+
   users.users.charles = {
     description = "Charles Ellis";
     isNormalUser = true;
@@ -12,4 +14,10 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICrtf2GddsSshWOjrpKK1uAq5MG9bsywctp8bTfwHSCl charles@hana"
     ];
   };
+
+  environment.variables = {
+    EDITOR = "vim";
+  };
+
+  programs.ssh.startAgent = true;
 }
