1167481011dbbfd224457e6dba9ce62a8a381f5c
Charles Ellis <hamled@hamled.dev>
2022-10-27 17:21:05 -0700

    charles emacs lsp-java VM args

--- users/dotfiles/charles/doom.d/config.el
+++ users/dotfiles/charles/doom.d/config.el
@@ -74,3 +74,11 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+
+(after! lsp-java
+  (setq
+    lsp-java-vmargs
+    `(,@lsp-java-vmargs
+       "-Xmx5G")))
+;;; config.el ends here
