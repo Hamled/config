12de3c3d4bd40b82bba0165c36032aabc463488d
Charles Ellis <hamled@hamled.dev>
2022-12-01 11:18:46 -0800

    nix path includes latest channel as nixpkgs-latest

--- modules/nixos/nix-path.nix
+++ modules/nixos/nix-path.nix
@@ -1,6 +1,7 @@
 { channel, inputs, ... }: {
   nix.nixPath = [
     "nixpkgs=${channel.input}"
+    "nixpkgs-latest=${inputs.latest}"
     "nixos-config=${../../lib/compat/nixos}"
     "home-manager=${inputs.home}"
   ];
