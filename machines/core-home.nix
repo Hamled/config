129b29157029ee6ffc982c60ca3f231d540f4fbe
Charles Ellis <hamled@hamled.dev>
2023-06-19 14:05:56 -0700

    hana uses home-manager

---
+++ machines/core-home.nix
@@ -0,0 +1,19 @@
+{ config, inputs, ... }: {
+  imports = [
+    inputs.home.nixosModules.home-manager
+    {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.extraSpecialArgs = inputs;
+      home-manager.sharedModules = [{
+        home.sessionVariables = {
+          NIX_PATH =
+            config.environment.sessionVariables.NIX_PATH or config.environment.variables.NIX_PATH;
+        };
+
+        xdg.configFile."nix/registry.json".text =
+          config.environment.etc."nix/registry.json".text;
+      }];
+    }
+  ];
+}
--- machines/hana/default.nix
+++ machines/hana/default.nix
@@ -4,5 +4,5 @@ let usersMods = builtins.map (config: config.nixos) (builtins.attrValues users);
 in nixpkgs.lib.nixosSystem {
   system = "x86_64-linux";
   specialArgs = { inherit inputs; };
-  modules = [ ../core.nix ./configuration.nix ] ++ usersMods;
+  modules = [ ../core.nix ../core-home.nix ./configuration.nix ] ++ usersMods;
 }
