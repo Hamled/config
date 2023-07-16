88d8a49a0f18df1d976700b6379def9d6058e921
Charles Ellis <hamled@hamled.dev>
2023-07-15 22:12:15 -0700

    Use flake-compat

--- flake.nix
+++ flake.nix
@@ -1,7 +1,11 @@
 {
   description = "NixOS configurations";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05"; };
+  inputs = {
+    flake-compat.url = "github:nix-community/flake-compat";
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  };
 
   outputs = inputs: { nixosConfigurations = { }; };
 }
