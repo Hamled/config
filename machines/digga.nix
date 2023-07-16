d6e4c3c4cb8773ba337da69ed5ca85726898dc0b
Charles Ellis <hamled@hamled.dev>
2023-07-16 12:24:00 -0700

    Remove Nix package version pin

--- machines/digga.nix
+++ machines/digga.nix
@@ -1,6 +1,4 @@
 { lib, pkgs, inputs, ... }: {
-  nix = { package = pkgs.nixVersions.nix_2_9; };
-
   # Hack to match old config
   system.configurationRevision = "58953c21e6f3ef2fbf7680d18567e9d7c9bf76eb";
 }
