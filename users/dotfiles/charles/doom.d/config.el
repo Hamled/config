850e0df867ed025719f7f4a6fcd47280f0978139
Charles Ellis <hamled@hamled.dev>
2026-04-07 11:56:46 -0700

    charles' emacs: Configure init opts for JDT.ls

--- users/dotfiles/charles/doom.d/config.el
+++ users/dotfiles/charles/doom.d/config.el
@@ -142,7 +142,16 @@
                  ((eq 'projectile (project-type project))
                   (funcall projectile-project-name-function
                            (project-root project)))
-                 (t (apply orig-fun (list project)))))))
+                 (t (apply orig-fun (list project))))))
+  (put 'projectile-project-name 'safe-local-variable #'stringp)
+
+  ;; Include external project roots defined in dir locals
+  (defvar projectile-external-roots nil)
+  (put 'projectile-external-roots 'safe-local-variable
+       (lambda (val)
+         (and (listp val) (-every #'stringp val))))
+  (cl-defmethod project-external-roots ((_project (head projectile)))
+    projectile-external-roots))
 
 (defun workspaces-project-unique-name-advice (orig-fun project-root)
   (let ((custom-name (funcall projectile-project-name-function
@@ -160,6 +169,23 @@
 (set-formatter! 'alejandra '("alejandra" "--quiet") :modes '(nix-mode nix-ts-mode))
 
 (after! eglot
+  (setf (alist-get '(java-mode java-ts-mode) eglot-server-programs nil nil #'equal)
+        `("jdtls-local" ; Prefer using local customized version of jdtls for Java
+          :initializationOptions
+          ,(lambda (server)
+             (list
+              ;; JDT.ls only accepts workspace folders from init options
+              ;; See: https://github.com/eclipse-jdtls/eclipse.jdt.ls/issues/1245
+              :workspaceFolders
+              (vconcat (mapcar (lambda (folder)
+                                 (plist-get folder :uri))
+                               (eglot-workspace-folders server)))
+              ;; JDT.ls doesn't automatically rebuild after didConfigurationChange
+              ;; however it does allow providing the workspace configuration as an
+              ;; init option, so the initial build can use the correct configuration
+              :settings
+              (eglot--workspace-configuration-plist server)))))
+
   (defun eglot-jdtls-project-config-update (server)
     "Send a signal to SERVER to update the Java project configuration.
      When called interactively, use the currently active server"
