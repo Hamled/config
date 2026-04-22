21f321efa65027fa323936ac5f9668ed03b18488
Charles Ellis <hamled@hamled.dev>
2026-04-22 08:01:01 -0700

    Use claude-agent-acp from nixpkgs

--- users/charles/nixos.nix
+++ users/charles/nixos.nix
@@ -31,22 +31,4 @@
   programs.openvpn3.enable = true;
 
   security.pam.services.swaylock = {};
-
-  nixpkgs.overlays = [
-    (final: prev: {
-      claude-agent-acp = prev.claude-agent-acp.overrideAttrs (finalAttrs: prevAttrs: {
-        version = "0.26.0";
-
-        src = prevAttrs.src.overrideAttrs (oldSrc: {
-          owner = "agentclientprotocol";
-          hash = "sha256-2G8gjMCnk3W1I2+4sNsumL15ts9bLXAOMguCmwnzWSA=";
-        });
-
-        npmDeps = prev.fetchNpmDeps {
-          inherit (finalAttrs) src;
-          hash = "sha256-msm4L8Yi7ma2eHOYXbZx+Qtrx4TzK7FV3HpVzRhQ19o=";
-        };
-      });
-    })
-  ];
 }
